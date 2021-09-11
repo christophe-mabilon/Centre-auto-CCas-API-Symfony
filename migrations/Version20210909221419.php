54743e05943718d6a6d8af45457973b55df57db9
Mabilon Christophe <70352608+christophe-mabilon@users.noreply.github.com>
2021-09-11 15:20:57 +0200

    mise a jour annonces et garage pour udate et create

---
+++ migrations/Version20210909221419.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210909221419 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE classified_ad CHANGE year year VARCHAR(255) NOT NULL, CHANGE car_doors car_doors VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE classified_ad CHANGE year year INT NOT NULL, CHANGE car_doors car_doors INT NOT NULL');
+    }
+}
