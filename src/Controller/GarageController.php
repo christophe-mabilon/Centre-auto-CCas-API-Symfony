07f80f39d47000b1cf65694f6d26a04e8a7a6b2b
Mabilon Christophe <70352608+christophe-mabilon@users.noreply.github.com>
2021-11-22 23:29:50 +0100

    rectif userCanEdit garage

--- src/Controller/GarageController.php
+++ src/Controller/GarageController.php
@@ -113,7 +113,7 @@ class GarageController extends AbstractController
         $jsonRecu = $req->getContent();
         $jsonRecu = $serializer->deserialize($jsonRecu, Garage::class, 'json');
         $isAdmin = in_array("ROLE_ADMIN", $currentUser->getRoles(), true);
-        dd($jsonRecu[userId]);
+        dd($jsonRecu->getUser()->getId());
         if($garage->getUser()->getId() === (int)$id){
             $userCanEdit = true;
             dd('ok can edit');
