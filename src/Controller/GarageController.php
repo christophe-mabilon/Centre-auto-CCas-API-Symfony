5666b6e2d5d51e6fc4ab3bf4162453a1f999215a
Mabilon Christophe <70352608+christophe-mabilon@users.noreply.github.com>
2021-11-23 00:49:08 +0100

    rectif userCanEdit garage

--- src/Controller/GarageController.php
+++ src/Controller/GarageController.php
@@ -107,11 +107,12 @@ class GarageController extends AbstractController
     /**
      * @Route("/garage/update/{id}", name="update_garage", methods={"PATCH"}, requirements={"id":"\d+"})
      */
-    public function update(Request $req,Garage $garage,UserInterface $currentUser, GarageRepository $repo, SerializerInterface $serializer,
-                           EntityManagerInterface $manager, $id): Response
-    {   $userCanEdit = false;
+    public function update(Request $req,Garage $garage,UserInterface $currentUser, GarageRepository $repo,
+                           SerializerInterface $serializer,EntityManagerInterface $manager): Response
+    {
         $jsonRecu = $req->getContent();
         $jsonRecu = $serializer->deserialize($jsonRecu, Garage::class, 'json');
+        $userCanEdit = false;
         $isAdmin = in_array("ROLE_ADMIN", $currentUser->getRoles(), true);
         $user = $this->getUser();
         $userGarage = $garage->getUser();
