f211b1eb0a7f2abe914537af17d2b1c15913d5fc
Mabilon Christophe <70352608+christophe-mabilon@users.noreply.github.com>
2021-11-22 04:47:11 +0100

    rectif userID annonce

--- src/Controller/ClassifiedAdController.php
+++ src/Controller/ClassifiedAdController.php
@@ -139,11 +139,10 @@ class ClassifiedAdController extends AbstractController
                 $model = $modelRepo->findOneBy(["id" => $data['model']]);
                 $region = $regionRepo->findOneBy(["id" => $data['region']]);
                 $garage = $garageRepo->findOneBy(["id" => $data['garage']]);
-                $user = $userRepo->findOneBy(["id" => $data['user']]);
+                $user = $userRepo->findOneBy(["id" => $data['userId']]);
                 $classifiedAd->setBrand($brand);
                 $classifiedAd->setModel($model);
                 $classifiedAd->setRegion($region);
-                $classifiedAd->setUser($this->getuser());
                 $classifiedAd->setGarage($garage);
                 $classifiedAd->setCreatedAt(new \DateTime());
                 $classifiedAd->setUpdatedOnAt(new \DateTime());
